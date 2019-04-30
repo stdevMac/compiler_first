d2626206f4e9edeb176db961e45c97b99f837a05
Marcos Maceo <marcosmaceowork@gmail.com>
2019-04-30 11:12:13 -0400

    basic project restructuration

---
+++ src/main/python/MyTableWidget.py
@@ -0,0 +1,75 @@
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (QPushButton, QTabWidget, QTextEdit,
+                             QVBoxLayout, QWidget, QFileDialog)
+
+from src.main.python.ShowTab import ShowTable
+
+
+class MyTableWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.resize(1028, 720)
+
+        s = QFont('SansSerif', 25)
+        self.setFont(s)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = ShowTable(self)
+        self.tabs.resize(300, 200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1, "Gramática")
+        self.tabs.addTab(self.tab2, "Resultados")
+
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.textBox = QTextEdit()
+        self.textBox.setPlainText("Twinkle, twinkle, little star,\n"
+                                  "How I wonder what you are.\n"
+                                  "Up above the world so high,\n"
+                                  "Like a diamond in the sky.\n"
+                                  "Twinkle, twinkle, little star,\n"
+                                  "How I wonder what you are!\n")
+        self.button = QPushButton('Computar', self)
+        self.button.setToolTip('This is an example button')
+        self.button.move(100, 70)
+        self.button.clicked.connect(self.on_click_button)
+        self.load = QPushButton('Cargar Archivo', self)
+        self.load.setToolTip('Carga un archivo .grm')
+        self.load.move(100, 70)
+        self.load.clicked.connect(self.file_open)
+
+        self.tab1.layout.addWidget(self.textBox)
+        self.tab1.layout.addWidget(self.button)
+        self.tab1.layout.addWidget(self.load)
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def file_open(self):
+        name = QFileDialog.getOpenFileName(self, 'Open File', filter="grm(*.grm)")
+        file = open(name[0], 'r')
+
+        with file:
+            text = file.read()
+            self.textBox.setPlainText(text)
+
+    @pyqtSlot()
+    def on_click_button(self):
+        print('PyQt5 button click')
+        print(self.textBox.toPlainText())
+        self.tabs.setCurrentIndex(1)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,6 @@
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import (QApplication, QPushButton, QTabWidget, QTextEdit,
-                             QVBoxLayout, QWidget, QMainWindow)
 import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from src.main.python.MyTableWidget import MyTableWidget
 
 
 class App(QMainWindow):
@@ -20,62 +18,6 @@ class App(QMainWindow):
         self.setCentralWidget(self.table_widget)
 
         self.show()
-
-
-class MyTableWidget(QWidget):
-
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
-        self.resize(1028, 720)
-
-        s = QFont('SansSerif', 25)
-        self.setFont(s)
-
-        # Initialize tab screen
-        self.tabs = QTabWidget()
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.resize(300, 200)
-
-        # Add tabs
-        self.tabs.addTab(self.tab1, "Gramática")
-        self.tabs.addTab(self.tab2, "Resultados")
-
-        # Create first tab
-        self.tab1.layout = QVBoxLayout(self)
-        self.textBox = QTextEdit()
-        self.textBox.setPlainText("Twinkle, twinkle, little star,\n"
-                                  "How I wonder what you are.\n"
-                                  "Up above the world so high,\n"
-                                  "Like a diamond in the sky.\n"
-                                  "Twinkle, twinkle, little star,\n"
-                                  "How I wonder what you are!\n")
-        self.button = QPushButton('PyQt5 button', self)
-        self.button.setToolTip('This is an example button')
-        self.button.move(100, 70)
-        self.button.clicked.connect(self.on_click_button)
-
-        self.tab1.layout.addWidget(self.textBox)
-        self.tab1.layout.addWidget(self.button)
-        self.tab1.setLayout(self.tab1.layout)
-
-        # self.setWindowState(Qt.WindowMaximized)
-
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
-
-    @pyqtSlot()
-    def on_click_button(self):
-        print('PyQt5 button click')
-        self.tabs.setCurrentIndex(1)
-
-    @pyqtSlot()
-    def on_click(self):
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
 
 if __name__ == '__main__':
