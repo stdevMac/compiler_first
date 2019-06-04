2aaed89e6b5d56ae7d3001e925e43c32c97765e2
Marcos Maceo <marcosmaceowork@gmail.com>
2019-06-04 10:00:33 -0400

    return tokens in tokenizer

--- src/main/python/tokenizer.py
+++ src/main/python/tokenizer.py
@@ -6,23 +6,29 @@ token_specification = [
     ('NO_TERMINAL', r'\w(?=-->)|\w([\t\r\f\v ])*(?=-->)'),  # Upper Case Letter
     ('ARROW', r'-->'),  # Arrow operator for productions
     ('PIPE', r'[|]'),  # Productions separator
-    ('EPSILON', r'\epsilon'),  # epsilon
+    ('EPSILON', r'epsilon'),  # epsilon
     ('PRODUCTION', r'([()[\]{}!"·%&/=*+-._?¿¡,;.:><a-zA-Z0-9_])+'),  # Production
     ('NEWLINE', r'\n'),  # Line endings
     ('WHITESPACE', r'[\t\r\f\v ]'),
-
 ]
-keywords = {'NO_TERMINAL', 'ARROW', 'PIPE', 'PRODUCTION', 'EPSILON'}
+
+
+def get_type(item):
+    for name, pattern in token_specification:
+        regs = re.compile(pattern)
+        if regs.match(item):
+            return name
+    return 1
 
 
 def tokenize(code: str):
     """
     input: Grammar like
-                    Z-->Ax|epsilon
-                    A       -->epsilon|Ass|esa
+                    Z --> Ax | epsilon
+                    A --> epsilon | Ass | esa
     Return: Collection of tokens
     """
-
+    tokens = []
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     line_num = 1
     line_start = 0
@@ -38,4 +44,5 @@ def tokenize(code: str):
         else:
             column = mo.start() - line_start
             value = value.lstrip().rstrip()
-            yield Token(kind, value, line_num, column)
+            tokens.append(Token(kind, value, line_num, column))
+    return tokens
